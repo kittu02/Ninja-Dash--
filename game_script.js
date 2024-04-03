6b7266d8a74b271e8418fdd4d92165810c01bbb6
KIRTAN <kirtan02y@gmail.com>
2024-04-03 11:07:14 +0530

    push

--- game_script.js
+++ game_script.js
@@ -209,9 +209,9 @@ function newPlatform(){
     width : platformW,
     height : platformH 
   }
-
   platformArray.push(platform);
 }
+
 function detectCollision(a,b){
   return a.x < b.x + b.width && //a's top left corner doesn't reach b's top right corner
     a.x + a.width > b.x && // a's top right corner passes b's top left corner
